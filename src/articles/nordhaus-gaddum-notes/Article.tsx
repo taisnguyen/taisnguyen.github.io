e1c718753ef9282de17c5136caa2dd1dac182293
Tai Sanh Nguyen <tai.sanh.ng@gmail.com>
2026-08-02 03:55:44 -0500

    feat: minor word choice in nordhaus-gaddum notes

--- src/articles/nordhaus-gaddum-notes/Article.tsx
+++ src/articles/nordhaus-gaddum-notes/Article.tsx
@@ -92,7 +92,7 @@ Without further ado, let us start by defining common neighborhoods.
 
 \bf{Definition 1.1.} Let $G$ be a simple graph and $S \subseteq V(G)$ be a subset of its vertices. Then the \it{common neighborhood} $\mathcal{C}_G(S)$ of $S$ in $G$ is defined by $\mathcal{C}_G(S) = \{ v \in V(G) : \forall s \in S, v \sim_G s \} = \bigcap_{s \in S} N_G(s)$, where $u \sim_G v$ means $u$ is adjacent to $v$ in $G$.
 
-Recall the definition of a dominating set and denote $\partial(G)$ as the number of dominating sets in $G$. As a reminder, we care about the following conjecture.
+Recall the definition of a dominating set and denote by $\partial(G)$ the number of dominating sets in $G$. As a reminder, we care about the following conjecture.
 
 \bf{Conjecture 1.2} (\cite{keough2019})\bf{.} For a graph $G$ on $n$ vertices, it holds that
 $$
@@ -105,7 +105,7 @@ How are common neighborhoods related to dominating sets?
 
 \it{Proof.} First suppose $S$ dominates $G$ and suppose otherwise that $\mathcal{C}_{\bar{G}}(S) \neq \emptyset$. Fix any $v \in \mathcal{C}_{\bar{G}}(S)$. Since $G$ is simple, we have that $v \notin S$. Since $S$ dominates $G$ but $v \notin S$, there must exist some $u \in S$ such that $u \sim_G v$. But then $u \nsim_{\bar{G}} v$, so it follows that $v \notin \mathcal{C}_{\bar{G}}(S)$, which is a contradiction. Now suppose that $\mathcal{C}_{\bar{G}}(S) = \emptyset$. If $S = V(G)$, we are done, so assume not and fix any $v \in V(G) \setminus S$. We claim that there exists some $u \in S$ such that $u \sim_G v$. Indeed, suppose otherwise. Then it follows that $u \sim_{\bar{G}} v$ for all $u \in S$, which implies that $v \in \mathcal{C}_{\bar{G}}(S)$, which is a contradiction since $\mathcal{C}_{\bar{G}}(S)$ is empty. \slot{qed}
 
-For a simple graph $G$, denote $\nu(G)$ as the number of subsets of $G$ that have a nonempty common neighborhood in $G$. That is, we define
+For a simple graph $G$, denote by $\nu(G)$ the number of subsets of $G$ that have a nonempty common neighborhood in $G$. That is, we define
 $$
 \nu(G) = |\{ S \subseteq V(G) : \mathcal{C}_G(S) \neq \emptyset \}|.
 $$
